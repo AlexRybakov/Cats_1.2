2d14c6d51bed1c464ea0713538eb29f7a7b44224
AlexRybakov <ralan1996@yandex.ru>
2023-03-03 19:53:08 +0400

    Cats_1.2

---
+++ src/cats.js
@@ -0,0 +1,75 @@
+const cats = [
+  {
+    "id": 1,
+    "name": "Шарик",
+    "favorite": false,
+    "rate": 4,
+    "age": 2,
+    "description": "Пугающий зверь",
+    "image": "http://file.mobilmusic.ru/e6/b6/09/1144826-240.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Тузик",
+    "favorite": false,
+    "rate": 2,
+    "age": 4,
+    "description": "Стандартный кот, любит всю еду",
+    "image": "https://wallpapers-fenix.eu/miniatura/210101/001935953.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Петька",
+    "favorite": false,
+    "rate": 3,
+    "age": 1,
+    "description": "Любитель приключений",
+    "image": "http://file.mobilmusic.ru/ba/65/a6/1418665-240.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Оксана",
+    "favorite": true,
+    "rate": 5,
+    "age": 3,
+    "description": "Королева дома",
+    "image": "https://cs8.pikabu.ru/avatars/2635/x2635265-334101763.png"
+  },
+  {
+    "id": 6,
+    "name": "Барс",
+    "favorite": true,
+    "rate": 5,
+    "age": 2,
+    "description": "Любитель наряжаться",
+    "image": "https://i.pinimg.com/280x280_RS/08/a4/6b/08a46bd038f9ab2f8aa501c3ddda752f.jpg"
+  },
+  {
+    "id": 7,
+    "name": "Кривик",
+    "favorite": false,
+    "rate": 2,
+    "age": 5,
+    "description": "Озабочен проблемами",
+    "image": "https://besthqwallpapers.com/Uploads/16-3-2018/44425/thumb-persian-cat-kitten-domestic-cats-pets-fluffy-kitten.jpg"
+  },
+  {
+    "id": 9,
+    "name": "Бусинка",
+    "favorite": false,
+    "rate": 5,
+    "age": 4,
+    "description": "Очень сильно любит шторы",
+    "image": "https://prodigits.co.uk/thumbs/wallpapers/p/animals/36/w9tyd40212632649.jpg"
+  },
+  {
+    "id": 10,
+    "name": "Цезарь",
+    "favorite": true,
+    "rate": 5,
+    "age": 6,
+    "description": "Великий",
+    "image": "https://www.zoovet.ru/text/577.jpg"
+  }
+];
+  
